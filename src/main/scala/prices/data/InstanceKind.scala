5935df4afe71d3a071a0d582a21d2442a5454c5b
Pishen Tsai <pishen02@gmail.com>
2024-04-25 13:18:00 +0800

    First attempt using AtomicCell

--- src/main/scala/prices/data/InstanceKind.scala
+++ src/main/scala/prices/data/InstanceKind.scala
@@ -1,3 +1,5 @@
 package prices.data
 
 final case class InstanceKind(getString: String) extends AnyVal
+
+case class InstancePrice(kind: String, price: Double, timestamp: String)
--- src/main/scala/prices/routes/InstanceKindRoutes.scala
+++ src/main/scala/prices/routes/InstanceKindRoutes.scala
@@ -7,23 +7,50 @@ import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 
-import prices.routes.protocol._
 import prices.services.InstanceKindService
+import cats.effect.std.AtomicCell
+import prices.data.InstancePrice
+import java.time._
 
-final case class InstanceKindRoutes[F[_]: Sync](instanceKindService: InstanceKindService[F]) extends Http4sDsl[F] {
+final case class InstanceKindRoutes[F[_]: Sync](instanceKindService: InstanceKindService[F], prices: AtomicCell[F, Map[String, InstancePrice]], expireInterval: Int) extends Http4sDsl[F] {
 
-  val prefix = "/instance-kinds"
+  implicit val instanceKindResponseEncoder = jsonEncoderOf[F, List[String]]
 
-  implicit val instanceKindResponseEncoder = jsonEncoderOf[F, List[InstanceKindResponse]]
+  object Kind extends QueryParamDecoderMatcher[String]("kind")
 
-  private val get: HttpRoutes[F] = HttpRoutes.of {
-    case GET -> Root =>
-      instanceKindService.getAll().flatMap(kinds => Ok(kinds.map(k => InstanceKindResponse(k))))
+  def isExpire(ip: InstancePrice): Boolean = {
+    val now = Instant.now.getEpochSecond
+    val priceTime = ZonedDateTime.parse(ip.timestamp).toInstant.getEpochSecond
+    now - priceTime > expireInterval
   }
 
-  def routes: HttpRoutes[F] =
-    Router(
-      prefix -> get
-    )
+  def tryUpdate(kind: String): F[Map[String, InstancePrice]] = {
+    prices.evalUpdateAndGet { priceMap =>
+      priceMap.get(kind).filterNot(isExpire) match {
+        case Some(_) => Sync[F].pure(priceMap)
+        case None => instanceKindService.getPrice(kind).map(ip => priceMap + (kind -> ip))
+      }
+    }
+  }
+
+  private val get: HttpRoutes[F] = HttpRoutes.of {
+    case GET -> Root / "instance-kinds" =>
+      instanceKindService.getAll().flatMap(kinds => Ok(kinds))
+    case GET -> Root / "prices" :? Kind(kind) =>
+      val price = for {
+        priceMap <- prices.get
+        updatedPriceMap <- priceMap.get(kind) match {
+          case Some(ip) =>
+            if (isExpire(ip)) tryUpdate(kind) else Sync[F].pure(priceMap)
+          case None => tryUpdate(kind)
+        }
+      } yield {
+        println("updatedPriceMap: " + updatedPriceMap)
+        updatedPriceMap.get(kind).map(_.price.toString).getOrElse("N/A")
+      }
+      Ok(price)
+  }
+
+  def routes: HttpRoutes[F] = Router("/" -> get)
 
 }
